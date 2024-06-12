141836e6aed8b34fc84aaf41f33d8018fd35e2af
Hanveshit <hanveshithreddy@gmail.com>
2024-06-12 17:52:22 +0530

    added routes related to donor

--- Backend/Routes/Donor.js
+++ Backend/Routes/Donor.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const router = express.Router();
+const route = express.Router();
 
+const {createDonationAppointment,DonationAppointments,LatestAppointment,CancelAppointment} = require('../Controllers/Donor/DonationAppointment');
 
+route.post('/create-donation-appointment',createDonationAppointment);
+route.get('/donation-appointments',DonationAppointments);
+route.get('/latest-appointment',LatestAppointment);
+route.delete('/cancel-appointment',CancelAppointment);
 
-module.exports = router;
+module.exports = route;
